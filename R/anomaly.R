00b2fe47253b811f2d4c98b7d843da6b362c8128
Danny Morris <dannymorris@github.com>
2018-05-24 21:15:05 -0400

    updates

--- R/anomaly.R
+++ R/anomaly.R
@@ -1,6 +1,7 @@
 LPF_outlier_score <- function(input_matrix, returns = 'scores', scale = TRUE,
                               error_agg_function = 'sum') {
-  #input_matrix <- iris[,-5]
+
+  input_matrix <- as.matrix(input_matrix)
   # prepare loop - N iterations
   n_cols <- ncol(input_matrix)
   # zero matrix - fill cols with errors[,i]
@@ -8,7 +9,7 @@ LPF_outlier_score <- function(input_matrix, returns = 'scores', scale = TRUE,
                          nrow = nrow(input_matrix))
   # loop through each col and predict from the rest
   for (i in 1:n_cols) {
-    linreg <- lm(input_matrix[,i] ~ ., data = input_matrix)
+    linreg <- lm(input_matrix[,i] ~ ., data = data.frame(input_matrix))
     error_matrix[,i] <- linreg$residuals
   }
   # aggregate residuals
