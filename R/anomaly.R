970b672eeb852f566d10a38e809d392e6daaa4e7
Danny Morris <dannymorris@github.com>
2018-05-26 13:31:26 -0400

    update ALSO

--- R/anomaly.R
+++ R/anomaly.R
@@ -1,32 +1,62 @@
-LPF_outlier_score <- function(input_matrix, returns = 'scores', scale = TRUE,
+ALSO <- function(input_matrix, returns = 'scores', scale = TRUE,
                               error_agg_function = 'sum') {
 
   input_matrix <- as.matrix(input_matrix)
+  input_df <- data.frame(input_matrix)
   # prepare loop - N iterations
   n_cols <- ncol(input_matrix)
   # zero matrix - fill cols with errors[,i]
-  error_matrix <- matrix(0, ncol = ncol(input_matrix),
+  error_matrix <- matrix(ncol = ncol(input_matrix),
                          nrow = nrow(input_matrix))
+
+  rmse_mat <- matrix(0, ncol=ncol(input_matrix), nrow=1)
+
+  k_folds <- createFolds(1:nrow(input_df), k=10)
+  cv_error_mat <- matrix(ncol=1, nrow=nrow(input_df))
+
   # loop through each col and predict from the rest
   for (i in 1:n_cols) {
-    linreg <- lm(input_matrix[,i] ~ ., data = data.frame(input_matrix))
-    error_matrix[,i] <- linreg$residuals
-  }
-  # aggregate residuals
-  aggregated_errors <- apply(error_matrix^2, 1, error_agg_function)
+    #linreg <- lm(input_matrix[,i] ~ ., data = data.frame(input_matrix))
+    # error_matrix[,i] <- linreg$residuals
 
-  if (returns == 'scores') {
-    if (scale == TRUE) {
-      scaled_aggregated_errors <- as.numeric(scale(aggregated_errors))
-      return(scaled_aggregated_errors)
-    } else {
-      return(aggregated_errors)
+    # each observation gets one out of sample prediction
+    for (j in 1:length(k_folds)) {
+      X <- as.matrix(input_df[-k_folds[[j]], -i])
+      Y <- as.matrix(input_df[-k_folds[[j]], i])
+      model_df <- data.frame(X,Y)
+      linreg <- lm(Y ~ ., data=model_df)
+      oos_predict <- predict(linreg, newdata = input_df[k_folds[[j]], -i])
+      oos_error <- (oos_predict - input_df[k_folds[[j]], i])^2
+      cv_error_mat[k_folds[[j]],] <- oos_error # get errors for jth variable
     }
+    error_matrix[,i] <- cv_error_mat
+    rmse_mat[,i] <- mean(cv_error_mat)
   }
 
-  if (return == "error_matrix") {
-    return(error_matrix)
-  }
+  adj_rmse <- ifelse(rmse_mat > 1, 1, rmse_mat)
+  weights <- 1-adj_rmse
+
+  scores <- apply(error_matrix, 1, function(x) {
+    sum(x*weights)
+  })
+
+  list(scores=scores,
+       error_matrix=error_matrix,
+       rmse_mat=rmse_mat,
+       weights=weights)
+
+#   if (returns == 'scores') {
+#     if (scale == TRUE) {
+#       scaled_aggregated_errors <- as.numeric(scale(aggregated_errors))
+#       return(scaled_aggregated_errors)
+#     } else {
+#       return(aggregated_errors)
+#     }
+#   }
+#
+#   if (return == "error_matrix") {
+#     return(error_matrix)
+#   }
 }
 
 nearest_neighbors <- function(data, d=NULL, ids, k) {
@@ -68,3 +98,5 @@ nearest_neighbors <- function(data, d=NULL, ids, k) {
     mutate(knn = row_number()) %>%
     filter(knn %in% k)
 }
+
+
