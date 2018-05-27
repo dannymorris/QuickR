893bced92f6d59190357119d46b86eb5642e7663
Danny Morris <dannymorris@github.com>
2018-05-27 00:29:25 -0400

    rf for ALSO

--- R/anomaly.R
+++ R/anomaly.R
@@ -1,62 +1,98 @@
-ALSO <- function(input_matrix, returns = 'scores', scale = TRUE,
-                              error_agg_function = 'sum') {
+ALSO <- function(data, method='rf', cv=FALSE, folds=NULL) {
 
-  input_matrix <- as.matrix(input_matrix)
-  input_df <- data.frame(input_matrix)
+  require(randomForest)
+  require(dplyr)
+
+  data <- data %>%
+    mutate_if(is.character, as.factor)
+
   # prepare loop - N iterations
-  n_cols <- ncol(input_matrix)
+  n_cols <- ncol(data)
   # zero matrix - fill cols with errors[,i]
-  error_matrix <- matrix(ncol = ncol(input_matrix),
-                         nrow = nrow(input_matrix))
+  error_matrix <- matrix(ncol = ncol(data),
+                         nrow = nrow(data))
 
-  rmse_mat <- matrix(0, ncol=ncol(input_matrix), nrow=1)
+  rmse_mat <- matrix(0, ncol=ncol(data), nrow=1)
 
-  k_folds <- createFolds(1:nrow(input_df), k=10)
-  cv_error_mat <- matrix(ncol=1, nrow=nrow(input_df))
+  if (method == 'lm' & cv == FALSE) {
+    message("cross validation set to TRUE for method='lm'")
+  }
+  cv <- ifelse(method == 'rf', cv, TRUE)
+
+  if (cv==TRUE) {
+    cv_error_mat <- matrix(ncol=1, nrow=nrow(data))
+    if(is.null(folds)) {
+      message("k_folds not supplided. default to 5")
+      k_folds <- createFolds(1:nrow(data), k=5)
+    } else {
+      k_folds <- createFolds(1:nrow(data), k=folds)
+    }
+  }
 
   # loop through each col and predict from the rest
   for (i in 1:n_cols) {
-    #linreg <- lm(input_matrix[,i] ~ ., data = data.frame(input_matrix))
-    # error_matrix[,i] <- linreg$residuals
 
-    # each observation gets one out of sample prediction
-    for (j in 1:length(k_folds)) {
-      X <- as.matrix(input_df[-k_folds[[j]], -i])
-      Y <- as.matrix(input_df[-k_folds[[j]], i])
-      model_df <- data.frame(X,Y)
-      linreg <- lm(Y ~ ., data=model_df)
-      oos_predict <- predict(linreg, newdata = input_df[k_folds[[j]], -i])
-      oos_error <- (oos_predict - input_df[k_folds[[j]], i])^2
-      cv_error_mat[k_folds[[j]],] <- oos_error # get errors for jth variable
+    # no CV (random forest only)
+    if (cv==FALSE) {
+      X <- as.matrix(data[, -i])
+      Y <- data[, i]
+      fit <- randomForest(x=X, y=Y)
+      if (class(Y) == 'factor') {
+        sq_error <- (as.numeric(fit$predicted) - as.numeric(Y))^2
+      } else {
+        sq_error <- (fit$predicted - Y)^2
+      }
+      error_matrix[,i] <- sq_error
+      rmse_mat[,i] <- mean(sq_error)
+
+    } else {
+
+      # CV
+      # each observation gets one out of sample prediction
+      for (j in 1:length(k_folds)) {
+
+        X_train <- as.matrix(data[-k_folds[[j]], -i])
+        Y_train <- data[-k_folds[[j]], i]
+        X_test <- data[k_folds[[j]], -i]
+        Y_test <- data[k_folds[[j]], i]
+
+        # random forest
+        if (method == 'rf') {
+          fit <- randomForest(x=X_train, y=Y_train)
+          oos_predict <- predict(fit, newdata = X_test)
+          if (class(Y) == 'factor') {
+            oos_sq_error <- (as.numeric(oos_predict) - as.numeric(Y_test))^2
+          } else {
+            oos_sq_error <- (oos_predict - Y_test)^2
+          }
+        }
+
+        # linear regression
+        if (method == 'lm') {
+          model_df <- data.frame(X_train, Y_train)
+          fit <- lm(Y_train ~ ., data=model_df)
+          oos_predict <- predict(fit, newdata = X_test)
+          oos_sq_error <- (oos_predict - Y_test)^2
+        }
+
+        cv_error_mat[k_folds[[j]],] <- oos_sq_error # get errors for jth variable
+      }
+      error_matrix[,i] <- cv_error_mat
+      rmse_mat[,i] <- mean(cv_error_mat)
     }
-    error_matrix[,i] <- cv_error_mat
-    rmse_mat[,i] <- mean(cv_error_mat)
   }
 
-  adj_rmse <- ifelse(rmse_mat > 1, 1, rmse_mat)
-  weights <- 1-adj_rmse
+  bounded_rmse <- ifelse(rmse_mat > 1, 1, rmse_mat)
+  feature_weights <- 1-bounded_rmse
 
   scores <- apply(error_matrix, 1, function(x) {
-    sum(x*weights)
+    sum(x*feature_weights)
   })
 
   list(scores=scores,
        error_matrix=error_matrix,
        rmse_mat=rmse_mat,
-       weights=weights)
-
-#   if (returns == 'scores') {
-#     if (scale == TRUE) {
-#       scaled_aggregated_errors <- as.numeric(scale(aggregated_errors))
-#       return(scaled_aggregated_errors)
-#     } else {
-#       return(aggregated_errors)
-#     }
-#   }
-#
-#   if (return == "error_matrix") {
-#     return(error_matrix)
-#   }
+       weights=feature_weights)
 }
 
 nearest_neighbors <- function(data, d=NULL, ids, k) {
